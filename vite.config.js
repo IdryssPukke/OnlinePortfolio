fdb3c292edd968efc2c082d48a232f91a0c6f0f1
Szymon Duda <szymon.duda@acceleroid.com>
2024-11-06 22:13:50 +0100

    added setup to project

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react-swc";
+import dotenv from "dotenv-safe";
+import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vitejs.dev/config/
+dotenv.config({
+  allowEmptyValues: true,
+});
+
 export default defineConfig({
-  plugins: [react()],
-})
+  define: {
+    "process.env": process.env,
+  },
+  plugins: [react(), svgr(), tsconfigPaths()],
+  server: {
+    port: 5173,
+  },
+});
